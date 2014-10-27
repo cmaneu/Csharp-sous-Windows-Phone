bc72d6fbd305d55ebaab48f7fdef3c3e36cff24c
Christopher MANEU <christopher@maneu.net>
2014-10-27 17:42:35 +0100

    Ajout exemple chapitre 12 - tests unitaires

---
+++ chapitre12/TestsUnitaires/TestsUnitaires.Tests/CalculPointsTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace TestsUnitaires.Tests
+{
+    [TestClass]
+    public class CalculPointsTests
+    {
+        [TestMethod]
+        public void CalculScore_Niveau1_SansBonus()
+        {
+            // Arrange
+            CalculPoints calcul = new CalculPoints();
+            calcul.NiveauDifficulte = 1;
+            int nombrePieces = 10;
+            int bonus = 0;
+            TimeSpan tempsJeu = TimeSpan.FromSeconds(184);
+            
+            // Act
+            int resultat = calcul.CalculScore(nombrePieces, bonus, tempsJeu);
+
+            // Assert
+            Assert.AreEqual(100, resultat);
+        }
+
+[TestMethod]
+public void CalculScore_Niveau1_BonusNegatif()
+{
+    // Arrange
+    CalculPoints calcul = new CalculPoints();
+    calcul.NiveauDifficulte = 1;
+    int nombrePieces = 10;
+    int bonus = -13;
+    TimeSpan tempsJeu = TimeSpan.FromSeconds(184);
+
+    // Act
+    // Assert
+    Assert.ThrowsException<ArgumentOutOfRangeException>(()=> calcul.CalculScore(nombrePieces, bonus, tempsJeu));
+}
+
+        [TestMethod]
+        public void CalculScore_Niveau1_NiveauDifficulteZero()
+        {
+            // Arrange
+            CalculPoints calcul = new CalculPoints();
+            calcul.NiveauDifficulte = 0;
+            int nombrePieces = 10;
+            int bonus = 13;
+            TimeSpan tempsJeu = TimeSpan.FromSeconds(184);
+
+            // Act
+            // Assert
+            Assert.ThrowsException<ArgumentNullException>(() => calcul.CalculScore(nombrePieces, bonus, tempsJeu));
+        }
+    }
+}
---
+++ chapitre12/TestsUnitaires/TestsUnitaires/CalculPoints.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsUnitaires
+{
+    public class CalculPoints
+    {
+        public int NiveauDifficulte { get; set; }
+
+        public int CalculScore(int piecesRecoltees, int bonus, TimeSpan tempsJeu)
+        {
+            if(piecesRecoltees < 0)
+                throw new ArgumentOutOfRangeException("piecesRecoltees");
+
+            if (bonus < 0)
+                throw new ArgumentOutOfRangeException("bonus");
+
+            int score = 0;
+            
+            score += piecesRecoltees*10;
+            score += bonus;
+
+            int pointsTempsJeu = (((int)tempsJeu.TotalMilliseconds) / (NiveauDifficulte * 10));
+
+            if (NiveauDifficulte > 3 && tempsJeu.TotalSeconds < 300)
+            {
+                score += pointsTempsJeu;            
+            }
+            
+            return score;
+        }
+    }
+}
