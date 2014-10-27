60f57aa86b8cdcf3f4c4daf7cf5aa5e85539cbb3
Christopher MANEU <christopher@maneu.net>
2014-10-27 17:19:00 +0100

    Ajout Exemple Chapitre 2 - Hello World

---
+++ chapitre2/HelloWorld/HelloWorld/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using Microsoft.Phone.Controls;
+
+namespace HelloWorld
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Bonjour " + tb_saisie.Text);
+        }
+    }
+}
